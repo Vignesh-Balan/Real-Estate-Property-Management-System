748cd6e0cfb94b9b262c6f1bc9a9dc202d82f41b
Vignesh-Balan <vigneshbalan1001@gmail.com>
2025-03-21 11:07:15 +0530

    updated apex classes and created base lwc

--- default/lwc/customPropertyList/customPropertyList.js
+++ default/lwc/customPropertyList/customPropertyList.js
@@ -1,3 +1,34 @@
 import { LightningElement } from 'lwc';
+import getPropertyList from '@salesforce/apex/PropertyListClass.getPropertyList';
 
-export default class CustomPropertyList extends LightningElement {}
+export default class CustomPropertyList extends LightningElement {
+
+    propList;
+    isDataLoaded = false;
+    message ="";
+
+    handleClick(){
+        this.isDataLoaded = false;
+        this.message = "";
+        getPropertyList({
+            startNumber:0,
+            inputPrice:0.0,
+            inputStatus:"",
+            inputFurnishingStatus:"",
+            inputLong:0.0,
+            inputLat:0.0
+        }).then( result => {
+            this.propList = JSON.parse(result);
+            if(this.propList == null){
+                this.message = "Reached end there is no property with the mentioned category."
+            }
+            else{
+                console.log('prop List' + this.propList);
+                this.isDataLoaded = true;
+            }
+            
+        }).catch( error => {
+            console.log('Error Occured '+ error);
+        });
+    }
+}
